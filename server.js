687b8f8d42c93207448119b915a1adfa5b6ccd96
zearanha <JAranha21@gmail.com>
2025-02-04 12:30:33 -0300

    Primeiro modelo

---
+++ server.js
@@ -0,0 +1,53 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+
+const prisma = new PrismaClient({
+    log: ['query', 'info', 'warn', 'error']
+  });  
+const app = express()
+app.use(express.json())
+
+
+
+app.get('/usuarios',async (req, res) =>{
+
+    const users = await prisma.user.findMany()
+
+    res.status(200).json(users)
+})
+
+app.post('/usuarios', async (req, res) => {
+    
+    const user = await prisma.user.create({
+        data:{
+            email: req.body.email,
+            age: req.body.age,
+            name: req.body.name
+        }
+    })
+   
+    
+
+    res.status(201).json(user)
+})
+
+
+
+app.listen(3000)
+
+
+
+//http://localhost:3000
+
+
+//req - requisição
+//res - resposta
+
+
+
+/*
+    Spider
+    LC2nRbnR29zabVlz
+
+*/
