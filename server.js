7836c59e14156babaa83a62ce627517412427580
zearanha <JAranha21@gmail.com>
2025-02-04 15:44:08 -0300

    nova versão

--- server.js
+++ server.js
@@ -1,40 +1,32 @@
-import express from 'express'
-import { PrismaClient } from '@prisma/client'
-
+import 'dotenv/config';
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient({
-    log: ['query', 'info', 'warn', 'error']
-  });  
-const app = express()
-app.use(express.json())
+    log: ['query', 'info', 'warn', 'error']
+});  
 
+const app = express();
+app.use(express.json());
 
-
-app.get('/usuarios',async (req, res) =>{
-
-    const users = await prisma.user.findMany()
-
-    res.status(200).json(users)
-})
+app.get('/usuarios', async (req, res) => {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+});
 
 app.post('/usuarios', async (req, res) => {
-    
     const user = await prisma.user.create({
-        data:{
+        data: {
             email: req.body.email,
             age: req.body.age,
             name: req.body.name
         }
-    })
-   
-    
+    });
+    res.status(201).json(user);
+});
 
-    res.status(201).json(user)
-})
+app.listen(3000);
 
-
-
-app.listen(3000)
 
 
 
@@ -48,6 +40,6 @@ app.listen(3000)
 
 /*
     Spider
-    LC2nRbnR29zabVlz
+    tQ3YyZwRqcg8Ay8A
 
 */
