acd9ed0f093d3fce7da7988784e742da3fb847dc
zearanha <JAranha21@gmail.com>
2025-02-06 19:19:15 -0300

    Modelo atualizado

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors'
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({
-    log: ['query', 'info', 'warn', 'error']
-});  
+const prisma = new PrismaClient();  
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.get('/usuarios', async (req, res) => {
     const users = await prisma.user.findMany();
@@ -24,6 +24,32 @@ app.post('/usuarios', async (req, res) => {
     });
     res.status(201).json(user);
 });
+
+
+app.put('/usuarios/:id', async (req, res) => {
+    const user = await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    });
+    res.status(200).json(user);
+});
+
+
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete({
+        where:{
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({mensage:'Usuario deletado'})
+})
 
 app.listen(3000);
 
